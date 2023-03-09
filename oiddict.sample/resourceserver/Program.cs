479cdf7d7d6ab185b81f1477dbfa50426eeb8bf8
Marco Milani <emme.milani@hotmail.it>
2023-03-09 09:21:19 +0100

    dev: add openiddict

--- oiddict.sample/resourceserver/Program.cs
+++ oiddict.sample/resourceserver/Program.cs
@@ -1,6 +1,25 @@
+using OpenIddict.Validation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Register the OpenIddict validation components.
+builder.Services.AddOpenIddict()
+    .AddValidation(options =>
+    {
+        // Note: the validation handler uses OpenID Connect discovery
+        // to retrieve the issuer signing keys used to validate tokens.
+        options.SetIssuer("https://localhost:7251/");
+
+        // Register the System.Net.Http integration.
+        options.UseSystemNetHttp();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+builder.Services.AddAuthentication(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
